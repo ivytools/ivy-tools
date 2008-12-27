6986668800304f1a9dfab23737abd6fcd0516ba4
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2008-12-27 00:59:38 +0000

    dbulla - addition of files to repository

---
+++ externalsFinder.cmd
@@ -0,0 +1,10 @@
+
+set JPTL_ROOT=d:\snapshots\java3rdpartylib\
+set JAVAEXTERNALS=d:\snapshots\javaexternals\trunk
+
+set IB_HOME=%JAVAEXTERNALS%\maintenance\IvyBrowser\webstart\lib
+
+set JAVA_HOME=%JPTL_ROOT%\jdk\1.5.0_15
+
+%JAVA_HOME%\bin\java -cp %IB_HOME%\swing-worker.jar;%IB_HOME%\ivyBrowser.jar;%IB_HOME%\xstream.jar;%IB_HOME%\svnkit.jar com.nike.externalsreporter.ui.MainFrame
+
