1312b07a4d65277b88ce129c925269b8b4531bda
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2009-01-28 06:01:55 +0000

    dbulla - Disabled autoparsing of repository after Curtis said it hung on startup

--- trunk/source/java/src/com/nurflugel/ivybrowser/ui/IvyBrowserMainFrame.java
+++ trunk/source/java/src/com/nurflugel/ivybrowser/ui/IvyBrowserMainFrame.java
@@ -7,31 +7,35 @@ import ca.odell.glazedlists.SortedList;
 import ca.odell.glazedlists.swing.EventTableModel;
 import ca.odell.glazedlists.swing.TableComparatorChooser;
 import ca.odell.glazedlists.swing.TextComponentMatcherEditor;
+
+import com.nurflugel.WebAuthenticator;
+
 import com.nurflugel.common.ui.Version;
+
 import com.nurflugel.ivybrowser.InfiniteProgressPanel;
 import com.nurflugel.ivybrowser.domain.IvyPackage;
 import com.nurflugel.ivybrowser.handlers.HtmlHandler;
-import com.nurflugel.WebAuthenticator;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.net.Authenticator;
+
+import java.util.List;
+import java.util.prefs.Preferences;
 
 import javax.swing.*;
-import static javax.swing.JOptionPane.showInputDialog;
 import static javax.swing.JOptionPane.*;
+import static javax.swing.JOptionPane.showInputDialog;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
-import java.awt.*;
-import java.awt.event.*;
-import java.net.Authenticator;
-import java.util.List;
-import java.util.prefs.Preferences;
 
 
 /** Created by IntelliJ IDEA. User: dbulla Date: Apr 26, 2007 Time: 12:37:50 PM To change this template use File | Settings | File Templates. */
 @SuppressWarnings({ "MethodParameterNamingConvention", "CallToPrintStackTrace", "MethodOnlyUsedFromInnerClass" })
 public class IvyBrowserMainFrame extends JFrame
 {
-
-    /** Use serialVersionUID for interoperability. */
     private static final long     serialVersionUID = 8982188831570838035L;
     private Cursor                busyCursor       = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
     private Cursor                normalCursor     = Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR);
@@ -39,14 +43,15 @@ public class IvyBrowserMainFrame extends JFrame
     private JButton               reparseButton    = new JButton("Reparse Repository");
     private JButton               quitButton       = new JButton("Quit");
     private JLabel                findLabel        = new JLabel("Find library:");
-    private JLabel                statusLabel      = new JLabel("Please wait while the repository is scanned...");
+    private JLabel                statusLabel      = new JLabel();
+//    private JLabel                statusLabel      = new JLabel("Please wait while the repository is scanned...");
     private JTable                resultsTable     = new JTable();
     private JTextField            libraryField     = new JTextField();
+    private Preferences           preferences      = Preferences.userNodeForPackage(IvyBrowserMainFrame.class);
+    private InfiniteProgressPanel progressPanel    = new InfiniteProgressPanel("Accessing the Ivy repository, please be patient");
+    public static final String   IVY_REPOSITORY   = "IvyRepository";
     private EventList<IvyPackage> repositoryList;
-    private Preferences preferences = Preferences.userNodeForPackage(IvyBrowserMainFrame.class);
-    private InfiniteProgressPanel progressPanel = new InfiniteProgressPanel("Accessing the Ivy repository, please be patient");
-    private static final String IVY_REPOSITORY = "IvyRepository";
-    private JPanel mainPanel;
+    private JPanel                mainPanel;
 
     // --------------------------- CONSTRUCTORS ---------------------------
 
@@ -56,13 +61,15 @@ public class IvyBrowserMainFrame extends JFrame
         pack();
         setSize(800, 600);
         BuilderMainFrame.centerApp(this);
-        //this was causing problems with GlazedLists throwing NPEs
-        //com.nurflugel.ivytracker.IvyBrowserMainFrame.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel",this);
+
+        // this was causing problems with GlazedLists throwing NPEs
+        // com.nurflugel.ivytracker.IvyBrowserMainFrame.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel",this);
         Authenticator.setDefault(new WebAuthenticator());
 
         libraryField.setEnabled(false);
         setVisible(true);
-         reparse();
+        //todo commented out for Curtis
+//        reparse();
     }
 
     private void initializeComponents()
@@ -95,7 +102,6 @@ public class IvyBrowserMainFrame extends JFrame
         mainPanel.add(statusLabel, BorderLayout.SOUTH);
 
         addListeners();
-        setCursor(busyCursor);
 
     }
 
@@ -122,7 +128,7 @@ public class IvyBrowserMainFrame extends JFrame
         specifyButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e)
                 {
-                    specifyRepository();
+                    specifyRepository(preferences);
                     reparse();
                 }
             });
@@ -138,33 +144,36 @@ public class IvyBrowserMainFrame extends JFrame
                     showIvyLine(e);
                 }
             });
-        addWindowListener(new WindowAdapter()
-        {
-            @Override
-            public void windowClosing(WindowEvent e)
-            {
-                super.windowClosing(e);
-                System.exit(0);
-            }
-        });
+        addWindowListener(new WindowAdapter() {
+                @Override public void windowClosing(WindowEvent e)
+                {
+                    super.windowClosing(e);
+                    System.exit(0);
+                }
+            });
     }
 
-    private void specifyRepository()
+    public static String specifyRepository(Preferences appPreferences)
     {
-        //todo get any stored preferences, display dialog, adn store results in preferences
-        String ivyRepositoryPath = preferences.get(IVY_REPOSITORY, "");
-        ivyRepositoryPath  = (String) showInputDialog(this, "What is the Ivy repository URL?", "Enter Ivy repository URL", QUESTION_MESSAGE, null,null,ivyRepositoryPath);
-        preferences.put(IVY_REPOSITORY,ivyRepositoryPath);
+        String ivyRepositoryPath = appPreferences.get(IVY_REPOSITORY, "");
+        ivyRepositoryPath = (String) showInputDialog(null, "What is the Ivy repository URL?", "Enter Ivy repository URL", QUESTION_MESSAGE, null, null, ivyRepositoryPath);
+        appPreferences.put(IVY_REPOSITORY, ivyRepositoryPath);
+        return ivyRepositoryPath;
     }
 
     private void reparse()
     {
+        setCursor(busyCursor);
+        
         progressPanel.start();
+
         String ivyRepositoryPath = preferences.get(IVY_REPOSITORY, "");
-        if(ivyRepositoryPath.length()>0){
-        HtmlHandler handler = new HtmlHandler(this,ivyRepositoryPath);
-        handler.execute();
-//        handler.doInBackground();}
+
+        if (ivyRepositoryPath.length() > 0) {
+            HtmlHandler handler = new HtmlHandler(this, ivyRepositoryPath);
+            handler.execute();
+
+            // handler.doInBackground();}
         }
     }
 
