cfa6bb687902941c756b61bdba775d2528bc7952
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2009-01-28 06:08:19 +0000

    dbulla - added timeouts on connections

--- trunk/source/java/src/com/nurflugel/ivybrowser/ui/IvyPackageFilterator.java
+++ trunk/source/java/src/com/nurflugel/ivybrowser/ui/IvyPackageFilterator.java
@@ -7,18 +7,19 @@ import com.nurflugel.ivybrowser.domain.IvyPackage;
 import java.util.List;
 
 
-/** Created by IntelliJ IDEA. User: douglasbullard Date: Jan 25, 2008 Time: 10:36:57 PM To change this template use File | Settings | File Templates. */
+/**
+ * Created by IntelliJ IDEA. User: douglasbullard Date: Jan 25, 2008 Time: 10:36:57 PM To change this template use File | Settings | File Templates.
+ */
 @SuppressWarnings({ "unchecked" })
 public class IvyPackageFilterator implements TextFilterator
 {
-    public void getFilterStrings(List   list,
-                                 Object o)
+    public void getFilterStrings(List list, Object o)
     {
         IvyPackage item = (IvyPackage) o;
         list.add(item.getModuleName());
         list.add(item.getVersion());
         list.add(item.getLibrary());
 
-         list.add(item.getOrgName());
+        list.add(item.getOrgName());
     }
 }
