df76c768ef4dd6d5ae0b7697daa5ac658adbc15c
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2009-10-04 23:09:35 +0000

    Added help files and jar

--- trunk/source/java/src/com/nurflugel/common/ui/Version.java
+++ trunk/source/java/src/com/nurflugel/common/ui/Version.java
@@ -4,7 +4,7 @@ package com.nurflugel.common.ui;
 public class Version
 {
   // don't adjust this manually - it's set by the build script
-  public static final String VERSION = "1.1.64";
+  public static final String VERSION = "1.1.65";
 
   private Version() {}
 }
