a8ca0b382994f11df74a1c13249ff46a4e4a72d2
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2009-10-08 05:55:29 +0000

    Added help to formatter, fixed bug in "Find Dot" button

--- source/java/src/com/nurflugel/common/ui/Version.java
+++ source/java/src/com/nurflugel/common/ui/Version.java
@@ -4,7 +4,7 @@ package com.nurflugel.common.ui;
 public class Version
 {
   // don't adjust this manually - it's set by the build script
-  public static final String VERSION = "1.1.88";
+  public static final String VERSION = "1.2.0";
 
   private Version() {}
 }
--- source/java/src/com/nurflugel/ivyformatter/ui/IvyFormatterMainFrame.java
+++ source/java/src/com/nurflugel/ivyformatter/ui/IvyFormatterMainFrame.java
@@ -2,6 +2,7 @@ package com.nurflugel.ivyformatter.ui;
 
 import com.nurflugel.common.ui.Version;
 import com.nurflugel.common.ui.Util;
+import static com.nurflugel.common.ui.Version.*;
 import com.nurflugel.ivybrowser.ui.BuilderMainFrame;
 import com.nurflugel.ivytracker.IvyTrackerMainFrame;
 import java.awt.*;
@@ -12,7 +13,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.URL;
 import javax.swing.*;
+import javax.help.HelpSet;
+import javax.help.HelpBroker;
+import javax.help.CSH;
+import javax.help.HelpSetException;
 
 /** Created by IntelliJ IDEA. User: douglasbullard Date: Jul 28, 2008 Time: 5:57:02 PM To change this template use File | Settings | File Templates. */
 @SuppressWarnings({"UseOfSystemOutOrSystemErr"})
@@ -24,12 +30,14 @@ public class IvyFormatterMainFrame extends JFrame
     private JButton quitButton;
     private JTextArea textArea;
     private JPanel contentPane = new JPanel();
+    private JButton helpButton;
+    public static final String HELP_HS = "ivyFormatterHelp.hs";
 
     public IvyFormatterMainFrame()
     {
         $$$setupUI$$$();
         setContentPane(contentPane);
-        setTitle("Ivy Beautifier v. " + Version.VERSION);
+        setTitle("Ivy Formatter v. " + VERSION);
         Util.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel", this);
         formatTextButton.addActionListener(new ActionListener()
         {
@@ -58,6 +66,29 @@ public class IvyFormatterMainFrame extends JFrame
         setSize(1000, 1000);
         BuilderMainFrame.centerApp(this);
         setVisible(true);
+        addHelpListener();
+    }
+
+    /** Add the help listener - link to the help files. */
+    private void addHelpListener()
+    {
+        ClassLoader classLoader = IvyFormatterMainFrame.class.getClassLoader();
+
+        try
+        {
+            URL hsURL = HelpSet.findHelpSet(classLoader, HELP_HS);
+
+            HelpSet helpSet = new HelpSet(null, hsURL);
+            HelpBroker helpBroker = helpSet.createHelpBroker();
+            CSH.DisplayHelpFromSource displayHelpFromSource = new CSH.DisplayHelpFromSource(helpBroker);
+
+            helpButton.addActionListener(displayHelpFromSource);
+        }
+        catch (HelpSetException ee)
+        {  // Say what the exception really is
+            System.out.println("Exception! " + ee.getMessage());
+
+        }
     }
 
     private void formatText(String ivyText)
@@ -132,6 +163,9 @@ public class IvyFormatterMainFrame extends JFrame
         gbc.gridy = 1;
         gbc.fill = GridBagConstraints.BOTH;
         contentPane.add(panel1, gbc);
+        helpButton = new JButton();
+        helpButton.setText("Help");
+        panel1.add(helpButton);
         formatTextButton = new JButton();
         formatTextButton.setText("Format Ivy Text");
         formatTextButton.setToolTipText("Paste text into the box or just click this button");
