065ed00cd3abf843ad1e4a5fa6f3eb1b22830fb8
douglas.bullard <douglas.bullard@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2011-01-06 03:20:07 +0000

    Removing branch

--- branches/trackerWork/source/java/src/com/nurflugel/externalsreporter/ui/Selectable.java
+++
@@ -1,8 +0,0 @@
-package com.nurflugel.externalsreporter.ui;
-
-/** Interface for an item which can be selected. */
-public interface Selectable
-{
-  boolean isSelected();
-  void setSelected(boolean selected);
-}
