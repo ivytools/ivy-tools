90ebc204030056ca739001d5de5833d2faaec20b
douglas.bullard@gmail.com <douglas.bullard@gmail.com@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2011-11-07 02:45:01 +0000

    Fixed password and username issues where username was blank on first use

--- source/java/src/com/nurflugel/common/ui/Version.java
+++ source/java/src/com/nurflugel/common/ui/Version.java
@@ -4,7 +4,7 @@ package com.nurflugel.common.ui;
 public class Version
 {
   // don't adjust this manually - it's set by the build script
-  public static final String VERSION = "2.0.40";
+  public static final String VERSION = "2.0.41";
 
   private Version() {}
 }
