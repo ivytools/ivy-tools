4e68bb9ed471beac3314483a021e8fc24d92dbd7
douglas.bullard@gmail.com <douglas.bullard@gmail.com@4f07411c-d3ad-11dd-8772-d1b041bf0e37>
2012-02-19 06:01:31 +0000

    Bypassing javac2 in Gradle

--- branches/gradle/whencejava.sh
+++ branches/gradle/whencejava.sh
@@ -3,7 +3,7 @@
 #OS X
 #set JAVA_HOME=/usr
 #solaris
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home/
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/
 #set PATH=$PATH:$JAVA_HOME/bin
 export PATH
 export JAVA_HOME
@@ -11,4 +11,4 @@ export ANT_OPTS=-Xmx640m
 
 set CLASSPATH=.
 #java -cp . WhenceJava $*
-java -cp webstart/lib/buildtasks-1.0-SNAPSHOT.jar com.nurflugel.buildtasks.WhenceJava $*
+java -cp webstart/lib/buildtasks-1.0-SNAPSHOT.jar:/nikeDev/java3rdpartylib/ant/1.8.0/lib/ant.jar com.nurflugel.buildtasks.WhenceJava $*
